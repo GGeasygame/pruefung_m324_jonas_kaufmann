e4debb5db27f0f1f47f64a9be3ef9903dec97587
Jonas Kaufmann <jonas.kaufmann@sunrise.ch>
2024-12-12 14:23:24 +0100

    linter test

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    {
+        rules: {
+            "no-unused-vars": "error",
+            "prefer-const": "error",
+            eqeqeq: ['error', 'always'],
+        },
+    },
+];
